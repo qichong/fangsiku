a09af2c95e1cd35cadb98ab2d5b5701c47c7201c
齐冲 <qichong@ishinetech.com>
2015-12-22 16:50:03 +0800

    首次上传

---
+++ sikuDemo/sikuDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sikuDemo
+//
+//  Created by 齐冲 on 15/12/22.
+//  Copyright © 2015年 齐冲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sikuDemo/sikuDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sikuDemo
+//
+//  Created by 齐冲 on 15/12/22.
+//  Copyright © 2015年 齐冲. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
